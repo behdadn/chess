5c8279082831adcf4df7efaaadcf3e78e85839e6
Behdad Nikfarjam <behdadn500@gmail.com>
2021-09-24 23:13:04 +0200

    added centipawn values

--- backend/src/engine.cpp
+++ backend/src/engine.cpp
@@ -1,9 +1,19 @@
 #include "engine.h"
 
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 #include "thc.h"
+
+std::unordered_map<char, int> centipawn = {
+    {'p', 100},
+    {'n', 350},
+    {'b', 350},
+    {'r', 525},
+    {'q', 1000},
+    {'k', 10000},
+};
 
 void display_position(thc::ChessRules &cr) {
     std::string fen = cr.ForsythPublish();
@@ -39,7 +49,7 @@ std::string eval(std::string fen_input) {
 // also includes following factors:
 // + bishop pair
 // - no pawns
-int material(thc::ChessRules cr) {
+int material(thc::ChessRules &cr) {
     int material_sum = 0;
 
     return material_sum;
