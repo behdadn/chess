24bfec72a05ff9010e442fe8db34396ccbcd7b16
Behdad Nikfarjam <behdadn500@gmail.com>
2021-09-24 18:04:57 +0200

    Added server

---
+++ backend/src/engine.cpp
@@ -0,0 +1,33 @@
+#include "engine.h"
+#include "thc.h"
+#include <iostream>
+#include <vector>
+
+void display_position(thc::ChessRules &cr) {
+    std::string fen = cr.ForsythPublish();
+    std::string s = cr.ToDebugStr();
+
+    std::cout << s.c_str() << std::endl;
+
+    std::cout << fen.c_str() << "\n" << std::endl;
+}
+
+std::string eval(std::string fen_input) {
+    thc::ChessRules cr;
+    cr.Forsyth(fen_input.c_str());
+
+    std::vector<thc::Move> moves;
+    std::vector<bool> check;
+    std::vector<bool> mate;
+    std::vector<bool> stalemate;
+    cr.GenLegalMoveList(moves, check, mate, stalemate);
+
+    std::cout << check.size() << std::endl;
+
+    thc::Move mv = moves[rand() % (moves.size() + 1)];
+    cr.PlayMove(mv);
+
+    display_position(cr);
+
+    return cr.ForsythPublish();
+}
