c342b7dbfcfea1c9b01940630315507904415b8b
Behdad Nikfarjam <behdadn500@gmail.com>
2021-09-25 15:25:35 +0200

    added functions to header

--- backend/src/engine.h
+++ backend/src/engine.h
@@ -1,3 +1,10 @@
 #include <string>
 
+#include "thc.h"
+
 std::string eval(std::string fen);
+int rootnm(thc::ChessRules &cr, int depth);
+int negamax(thc::ChessRules &cr, int depth);
+int heuristic_eval(thc::ChessRules &cr);
+int material_eval(thc::ChessRules &cr);
+thc::ChessRules fen_to_cr(std::string fen);
