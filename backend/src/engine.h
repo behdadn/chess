56c425f734e0e235f6b0db3dee9d5cc6199b8767
Behdad Nikfarjam <behdadn500@gmail.com>
2021-09-25 23:08:00 +0200

    renamed functions

--- backend/src/engine.h
+++ backend/src/engine.h
@@ -1,10 +1,12 @@
+#pragma once
+
 #include <string>
 
 #include "thc.h"
 
-std::string eval(std::string fen);
+thc::ChessRules fen_to_cr(std::string fen);
+std::string calculate_move(std::string fen);
 int rootnm(thc::ChessRules &cr, int depth);
 int negamax(thc::ChessRules &cr, int depth);
-int heuristic_eval(thc::ChessRules &cr);
+int eval(thc::ChessRules &cr);
 int material_eval(thc::ChessRules &cr);
-thc::ChessRules fen_to_cr(std::string fen);
