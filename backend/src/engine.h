be17aca0a62579a4fc671f56b79382cb413e31b4
Behdad Nikfarjam <behdadn500@gmail.com>
2021-10-05 13:37:26 +0200

    updated

--- backend/src/engine.h
+++ backend/src/engine.h
@@ -5,8 +5,9 @@
 #include "thc.h"
 
 thc::ChessRules fen_to_cr(std::string fen);
-std::string calculate_move(std::string fen);
-int rootnm(thc::ChessRules &cr, int depth);
-int negamax(thc::ChessRules &cr, int depth);
+std::string calculate_move(std::string fen, int depth);
+int rootmm(thc::ChessRules &cr, int depth);
+int minimax(thc::ChessRules &cr, int depth);
 int eval(thc::ChessRules &cr);
 int material_eval(thc::ChessRules &cr);
+double psqt_eval(thc::ChessRules &cr);
